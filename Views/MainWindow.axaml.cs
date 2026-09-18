358fd7a7e0393794e94994161448b587420c0712
Paolo <paolohagos08@gmail.com>
2026-09-18 22:42:22 +0800

    Add ui & logic

--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace TempConverterApp.Views;
 
@@ -7,5 +8,33 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void Button_OnClick(object? sender, RoutedEventArgs e)
+    {
+        ConvertTemp();
+    }
+
+    private void Celsius_TextChanged(object? sender, TextChangedEventArgs e)
+    {
+        ConvertTemp();
+    }
+
+    private void ConvertTemp()
+    {
+        if (string.IsNullOrEmpty(Celsius.Text) || Celsius.Text == "-")
+        {
+            Fahrenheit.Text = "";
+        } 
+        else if (double.TryParse(Celsius.Text, out double C))
+        {
+            var F = C * (9d / 5d) + 32;
+            Fahrenheit.Text = F.ToString("0.0");
+        }
+        else
+        {
+            Celsius.Text = "0";
+            Fahrenheit.Text = "0";
+        }
     }
 }
