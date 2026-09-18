e14ab9e340ad96509d418bb34fcc4d1b24d3e3f2
Paolo <paolohagos08@gmail.com>
2026-09-18 18:36:20 +0800

    Initial commit

---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace TempConverterApp.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
---
+++ Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace TempConverterApp.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
